425713c29a2fde41b60d56d1f48f79cc82e620e3
Brian Grana <130684373+azurerazor@users.noreply.github.com>
2023-10-20 00:09:29 -0400

    Create DSU.h

---
+++ Implementations/Data Structures/DSU.h
@@ -0,0 +1,24 @@
+/*
+* Source: KACTL
+* Desc: Maintains union of disjoint sets in O(alpha(n)) amortized
+* Test: N/A
+*/
+
+#pragma once
+
+struct DSU {
+    vector<int> s;
+    int groups;
+    void init(int n) {s = vector<int>(n, -1); groups = n;};
+    int find(int v) { return ((s[v] < 0) ? v : s[v] = find(s[v]));}
+    int size(int v) { return -s[find(v)]; }
+    
+    bool join(int a, int b) {
+        a = find(a); b = find(b);
+        if (a == b) return false;
+        if (s[a] > s[b]) swap(a, b);
+        s[a] += s[b]; s[b] = a;
+        groups--;
+        return true;
+    }
+};
