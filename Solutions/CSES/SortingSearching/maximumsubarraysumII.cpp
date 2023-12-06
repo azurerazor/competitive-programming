b62b2757b78f2728e40842ebb3ffe60ec67254d5
Brian G <130684373+azurerazor@users.noreply.github.com>
2023-12-06 12:44:30 -0500

    Add files via upload

---
+++ Solutions/CSES/SortingSearching/maximumsubarraysumII.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    vector<ll> ps(n+2, 0);
+
+    for (int i = 1; i <= n; i++) {
+        ll v; cin >> v;
+        ps[i] = ps[i-1] + v;
+    }
+
+    multiset<ll> minpfx;
+    ll ans = -1e18;
+
+    for (int l = a-b, r = 0; r+a <= n; l++, r++) {
+        minpfx.insert(ps[r]);
+        if (l > 0) minpfx.erase(minsfx.find(ps[l-1]));
+        ans = max(ans, ps[r + a] - *minpfx.begin());
+    }
+
+    cout << ans << '\n';
+    return 0;
+}
