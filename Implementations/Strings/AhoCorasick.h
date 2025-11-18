fcfbb4112ec0bbc838e89fab6d5c98df3f0a1256
Brian G <130684373+azurerazor@users.noreply.github.com>
2025-11-17 19:49:28 -0500

    golf aho

--- Implementations/Strings/AhoCorasick.h
+++ Implementations/Strings/AhoCorasick.h
@@ -6,13 +6,15 @@ struct node {
     int prv=0, link=0, len=0;
     char c;
     vi ids;
-    node() {
+    node(int p, int k, int l, char c): 
+        prv(p), link(k), len(l), c(c) {
         memset(nxt, 0, sizeof(nxt));
     }
 };
 
 vector<node> build(vector<string>& s) {
-    vector<node> aho(1);
+    vector<node> aho;
+    aho.emplace_back(0, 0, 0, 0);
     queue<tuple<int, int, int>> q;
     for (int i = 0; i < sz(s); i++) {
         q.emplace(0, i, 0);
@@ -22,11 +24,8 @@ vector<node> build(vector<string>& s) {
         char cc = s[i][p] - FL;
         if (aho[n].nxt[cc] < aho[n].len+1) {
             int j = sz(aho);
-            aho.emplace_back();
-            aho[j].len = aho[n].len+1;
-            aho[j].prv = n;
-            aho[j].c = cc;
-            aho[j].link = aho[aho[n].link].nxt[cc];
+            aho.emplace_back(n, aho[aho[n].link].nxt[cc],
+                 aho[n].len+1, cc);
             aho[n].nxt[cc] = j;
             for (int k = 0; k < ALPHA; k++) {
                 aho[j].nxt[k] = aho[aho[j].link].nxt[k];
