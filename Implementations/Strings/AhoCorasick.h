ead49d569fa2b08c4a3f52865d7e1b9224b59377
Brian G <130684373+azurerazor@users.noreply.github.com>
2025-11-19 13:51:42 -0500

    add nic golfs

--- Implementations/Strings/AhoCorasick.h
+++ Implementations/Strings/AhoCorasick.h
@@ -8,40 +8,33 @@
 #define FL 'a'
  
 struct node {
-    int nxt[ALPHA];
-    int prv=0, link=0, len=0;
-    vi ids;
-    node(int p, int k, int l): 
-        prv(p), link(k), len(l) {
-        memset(nxt, 0, sizeof(nxt));
-    }
+    array<int, ALPHA> nxt;
+    int prv, link, len; vi ids;
+    node(int p, int k, int l) : prv(p), link(k), len(l) {
+        fill(all(nxt), 0); }
 };
- 
+
 vector<node> build(vector<string>& s) {
-    vector<node> aho;
-    aho.emplace_back(0, 0, 0);
+    vector<node> aho{{0, 0, 0}};
     aho[0].ids = vi(sz(s));
     iota(all(aho[0].ids), 0);
 
     for (int u = 0; u < sz(aho); u++) {
-        for (int i = 0; i < ALPHA; i++)
-            aho[u].nxt[i] = aho[aho[u].link].nxt[i];
+        aho[u].nxt = aho[aho[u].link].nxt;
         for (int i = 0; i < sz(aho[u].ids); i++) {
-            int c = aho[u].ids[i];
-            if (aho[u].len < sz(s[c])) {
-                int ch = s[c][aho[u].len] - FL;
-                if (aho[aho[u].nxt[ch]].len < aho[u].len+1) {
+            int c = aho[u].ids[i], L = aho[u].len;
+            if (L < sz(s[c])) {
+                int ch = s[c][L] - FL;
+                if (aho[aho[u].nxt[ch]].len < L+1) {
                     int j = sz(aho);
-                    aho.emplace_back(u, aho[aho[u].link].nxt[ch], aho[u].len+1);
+                    aho.emplace_back(u, aho[aho[u].link].nxt[ch], L+1);
                     aho[u].nxt[ch] = j;
                 }
-                swap(aho[u].ids[i], aho[u].ids[sz(aho[u].ids) - 1]);
+                swap(aho[u].ids[i--], aho[u].ids.back());
                 aho[u].ids.pop_back();
-                i--;
                 aho[aho[u].nxt[ch]].ids.push_back(c);
             }
         }
     }
- 
+
     return aho;
-}
